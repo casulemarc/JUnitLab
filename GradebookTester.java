bae8aa8cb550e3abfe6018078d6d578dc2c84d7c
Mark Kasule <75389946+mlkasule@users.noreply.github.com>
2021-09-22 01:31:06 -0400

    Add files via upload

---
+++ GradebookTester.java
@@ -0,0 +1,112 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author mkasulecoder
+ *
+ */
+class GradebookTester {
+
+	private GradeBook gradeTest_1;
+	private GradeBook gradeTest_2;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+
+		gradeTest_1 = new GradeBook(5);
+		gradeTest_2 = new GradeBook(5);
+
+		gradeTest_1.addScore(23);
+		gradeTest_1.addScore(40);
+		gradeTest_1.addScore(56);
+
+		gradeTest_2.addScore(30);
+		gradeTest_2.addScore(93);
+		gradeTest_2.addScore(97);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+
+		gradeTest_1 = null;
+		gradeTest_2 = null;
+	}
+
+	@Test
+	void testAddScore() {
+
+		// grade 1
+		String gradeActualOutput1 = gradeTest_1.toString();
+
+		// grade 2
+		String gradeActualOutput2 = gradeTest_2.toString();
+
+		assertTrue(gradeActualOutput1.equals("23.0 40.0 56.0"));
+		assertTrue(gradeActualOutput2.equals("30.0 93.0 97.0"));
+
+		// Score sizes
+		int scoreSize1 = gradeTest_1.getScoreSize();
+		assertEquals(3, scoreSize1);
+
+		int scoreSize2 = gradeTest_2.getScoreSize();
+		assertEquals(3, scoreSize2);
+
+	}
+
+	/*
+	 * Test for the sum of the scores
+	 */
+	@Test
+	void testSum() {
+
+		double sum1 = gradeTest_1.sum();
+		double sum2 = gradeTest_2.sum();
+
+		assertEquals(119.0, sum1, 0.0001);
+		assertEquals(220.0, sum2, 0.0001);
+
+	}
+
+	/*
+	 * Test for the minimum score, or 0 if there are no scores.
+	 */
+
+	@Test
+	void testMinimum() {
+
+		double minimumGrade1 = gradeTest_1.minimum();
+		double minimumGrade2 = gradeTest_2.minimum();
+
+		assertEquals(23.0, minimumGrade1, 0.001);
+		assertEquals(30.0, minimumGrade2, 0.001);
+	}
+
+	/*
+	 * Test if the sum of the scores, with the lowest score dropped if there are at
+	 * least two scores, or 0 if there are no scores.
+	 */
+	@Test
+	void testFinalScore() {
+
+		double finalScore1 = gradeTest_1.finalScore();
+		double finalScore2 = gradeTest_2.finalScore();
+
+		assertEquals(96.0, finalScore1, 0.001);
+		assertEquals(190.0, finalScore2, 0.0001);
+	}
+
+}
